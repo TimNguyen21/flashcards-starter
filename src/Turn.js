8b9f482ffabcea3a37ce9debc6a760169c13d89f
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-01-28 17:29:51 -0700

    Complete functionality for Turn.js

--- src/Turn.js
+++ src/Turn.js
@@ -2,6 +2,7 @@ class Turn {
   constructor(answer, card) {
     this.answer = answer;
     this.card = card;
+    this.isCorrect = null;
   }
 
   returnGuess() {
@@ -13,14 +14,22 @@ class Turn {
   }
 
   evaluateGuess() {
-    
+    if (this.answer === this.card.correctAnswer) {
+      this.isCorrect = true;
+      return true;
+    } else {
+      this.isCorrect = false;
+      return false;
+    }
   }
 
   giveFeedback() {
-
+    if (this.isCorrect === false) {
+      return 'incorrect!';
+    } else if (this.isCorrect === true){
+      return 'correct!';
+    }
   }
 }
-
-
 
 module.exports = Turn
