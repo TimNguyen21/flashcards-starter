01cb7b8788286a21ca92bc4b099f1698b4ecfa51
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-01-28 17:30:35 -0700

    Complete test for Turn.js with Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -40,4 +40,19 @@ describe('Turn', function() {
     const turn = new Turn('pug', card);
     expect(turn.returnCard()).to.deep.equal({id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter'});
   });
+
+  it('should return false for incorrect answer', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should return feedback of incorrect when guess wrong', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+
+    turn.evaluateGuess();
+
+    expect(turn.giveFeedback()).to.equal('incorrect!');
+  });
 });
