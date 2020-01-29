0393c6b688d2df9de52171bce547b6d050b48e79
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-01-29 13:55:30 -0700

    Set up the Round and Round-test js

---
+++ src/Round.js
@@ -0,0 +1,26 @@
+class Round {
+  constructor(deck, turn, incorrectGuess) {
+    this.deck = deck;
+    this.turns = turn;
+    this.incorrectGuesses = incorrectGuess || [];
+
+  }
+
+  returnCurrentCard() {
+
+  }
+
+  calculatePercentCorrect() {
+
+  }
+
+  endRound() {
+
+  }
+
+  calculatePercentCorrect() {
+
+  }
+}
+
+module.exports = Round
